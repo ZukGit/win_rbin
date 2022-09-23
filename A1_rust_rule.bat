fcfe3bee38411ffec7b7f48c40530ca36a71153c
ZukGit <382581427@qq.com>
2022-09-23 14:09:30 +0800

    2022年09月23日-14时09分29秒______Zukgit-的提交

--- A1_rust_rule.bat
+++ A1_rust_rule.bat
@@ -17,4 +17,10 @@ cd  /d  %dp0%/A1_rust_rule
 
 
 %HOME%\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin\cargo.exe build 
+echo errorlevel=%ERRORLEVEL%
+if %ERRORLEVEL% LEQ 1 (
 %A1_rust_rule%\target\debug\A1_rust_rule.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+echo   _________________ Success Compile and Run ! _________________
+) else (
+echo   _________________ Failed Compile , Please Check ! _________________
+)
